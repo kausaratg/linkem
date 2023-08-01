a25e13555a36406aa8c8ad1511f9c12c5c662126
kassy <kausaratg@gmail.com>
2023-08-01 01:25:51 +0100

    improved templates, worked on add pdf feature

--- add_link/urls.py
+++ add_link/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from add_link.views import index, addlink_views, generate_link
+from add_link.views import index, addlink_views, generate_link,  get_pdf
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("addlink/", addlink_views, name="addlink"),
     path("linkem/<str:username>/", generate_link, name="generatelink"),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("get_pdf/<str:username>/", get_pdf, name="get_pdf"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- add_link/views.py
+++ add_link/views.py
@@ -3,6 +3,9 @@ from add_link.forms import AddlinkForm
 from django.contrib import messages
 from add_link.models import Add_link
 from django.contrib.auth.decorators import login_required
+from add_link.process import html_to_pdf
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 # Create your views here.
 @login_required(login_url='login')
@@ -27,8 +30,17 @@ def addlink_views(request):
         return redirect('/')
     return render(request, 'add_link/form.html', {"form":form})
 
-def generate_link(request, username):
-    links = Add_link.objects.filter(username = request.user)
+def get_pdf(request, username):
+    links = Add_link.objects.filter(username = request.user)  
     user = request.user
     context = {'links':links, "user":user}
-    return render(request, "add_link/generate_link.html", context)
+    open('templates/add_link/my_link', "w").write(render_to_string('add_link/generate_link.html', {'links': links, 'user':user}))
+
+    pdf = html_to_pdf('add_link/my_link')
+    return HttpResponse(pdf, content_type='application/pdf')
+
+def generate_link(request, username):
+    links = Add_link.objects.filter(username = request.user)  
+    user = request.user
+    context = {'links':links, "user":user}
+    return render(request, 'add_link/generate_link.html', context)
